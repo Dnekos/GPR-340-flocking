bd336efbdc06d247ad89ca67032f9ffda9b96c4b
Demetrius Nekos <demetrius.nekos@mymail.champlain.edu>
2022-09-05 18:33:18 -0400

    Cohesion and Alignment rules

--- examples/flocking/behaviours/AlignmentRule.cpp
+++ examples/flocking/behaviours/AlignmentRule.cpp
@@ -4,8 +4,14 @@
 Vector2 AlignmentRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
     Vector2 averageVelocity = Vector2::zero();
 
-    // todo: add your code here to align each boid in a neighborhood
-    // hint: iterate over the neighborhood
+    if (neighborhood.size() > 0) {
+        for (const auto& neighbor : neighborhood) {
+            if (boid != neighbor) {
+                averageVelocity += neighbor->getVelocity();
+            }
+        }
+        averageVelocity /= neighborhood.size();
+    }
 
     return Vector2::normalized(averageVelocity);
 }
--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -2,10 +2,22 @@
 #include "../gameobjects/Boid.h"
 
 Vector2 CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
-    Vector2 cohesionForce;
+    Vector2 cohesionForce = Vector2::zero();
+    float cohesionMagnitude = 5;
 
-    // todo: add your code here to make a force towards the center of mass
-    // hint: iterate over the neighborhood
+    if (neighborhood.size() > 0)
+    {
+        Vector2 centerOfMass;
+        for (const auto& neighbor : neighborhood) {
+            if (boid != neighbor) {
+                centerOfMass += neighbor->getPosition();
+            }
+        }
+
+        Vector2 distanceToBoid = (centerOfMass / neighborhood.size()) - boid->getPosition();
+        float distance = Vector2::getDistance((centerOfMass / neighborhood.size()), boid->getPosition());
+        cohesionForce = distanceToBoid.normalized() * (distance * boid->getDetectionRadius()) * cohesionMagnitude;
+    }
 
     return cohesionForce;
 }
